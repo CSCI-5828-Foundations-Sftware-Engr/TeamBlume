0a21e7f51bc59d6d59d8f42b7be3896167ddb924
hjoshi123 <mail2hemantjoshi@pm.me>
2023-03-23 17:23:14 -0600

    Fixed lint

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:node/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelrc: false,
+    requireConfigFile: false,
+    presets: ['@babel/preset-env'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-nested-ternary': 0,
+    'import/order': 0,
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'react/no-array-index-key': 0,
+    'react/prefer-stateless-function': 0,
+    'react/sort-comp': 0,
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: false, variables: false },
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', 'jsx'],
+      },
+    ],
+    'react/forbid-prop-types': 0,
+    'react/prop-types': 0,
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-plusplus': 'off',
+    'no-process-exit': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-dynamic-require': 'off',
+    'global-require': 'off',
+    'consistent-return': 'off',
+    // 'max-len': ['error', 80],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
