095eba0f04cc7c8550d319d1ad6dcb5081756fa0
Sriharsha Reddy Yarasani <71772060+sriharsharedd-y@users.noreply.github.com>
2023-04-16 16:42:55 -0600

    UI test implementation using Cypress
    * Added footer tests for the app using Cypress

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="footer">
       <div className=""></div>
       <div className="">
-        <span className="text-sm">Copyright PACom</span>
+        <span className="footer-text text-sm">Copyright PACom</span>
       </div>
       <div className="flex-end flex"></div>
     </div>
