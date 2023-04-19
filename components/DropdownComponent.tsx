2d56ebc8eabb032e68b5cdc2a13ebd779b601fa2
Sriharsha Reddy Yarasani <71772060+sriharsharedd-y@users.noreply.github.com>
2023-04-19 14:30:15 -0600

    UI Fixes
    * UI Fixes to fix the deployment

--- components/DropdownComponent.tsx
+++ components/DropdownComponent.tsx
@@ -3,31 +3,42 @@ import { Dropdown } from "@nextui-org/react";
 
 export default function DropdownComponent({ddType,ddItems} : {ddType : string, ddItems : {key: string; name : string;}[]}) {
 
-    const [selected, setSelected] = React.useState(new Set([ddItems[0].key] || "Select an option"));
+  type ddItemObj = {
+    key?: string;
+    name?: string;
+  };
+  
+  const [selected, setSelected] = React.useState(new Set([ddItems[0].key]));
 
-    const selectedValue = React.useMemo(
-      () => Array.from(selected).join(", ").replaceAll("_", " "),
-      [selected]
-    );
+  const selectedValue = React.useMemo(
+    () => Array.from(selected).join(", ").replaceAll("_", " "),
+    [selected]
+  );
 
-    const cName = ddType + " drop-down-selection";
-    const iId = ddType + "-value";
+  function handleSelected(e: string | Set<React.Key>){
+    setSelected(e as Set<string>);
+  }
 
-    return(
-      <div className="dropdown-selector" id="dropdown-selector">
-        <Dropdown>
-          <Dropdown.Button className={cName} value={selectedValue}>{selectedValue}</Dropdown.Button>
-          <Dropdown.Menu aria-label="Dynamic Actions" variant="solid" selectionMode="single" 
-          items={ddItems} selectedKeys={selected} onSelectionChange={setSelected}
-          >
-            {(item) => (
-              <Dropdown.Item key={item.key}>
-                {item.name}
-              </Dropdown.Item>
-            )}
-          </Dropdown.Menu>
-        </Dropdown>
-        <input type="hidden" id={iId} value={selectedValue} />
-      </div>
-    );
+  const cName = ddType + " drop-down-selection";
+  const iId = ddType + "-value";
+
+  return(
+    <div className="dropdown-selector" id="dropdown-selector">
+      <Dropdown>
+        <Dropdown.Button className={cName} value={selectedValue}>{selectedValue}</Dropdown.Button>
+        <Dropdown.Menu aria-label="Dynamic Actions" variant="solid" selectionMode="single" 
+        items={ddItems} selectedKeys={selected} 
+        onSelectionChange={(e) => {handleSelected(e)}}
+        
+        >
+          {(item : ddItemObj) => (
+            <Dropdown.Item key={item.key}>
+              {item.name}
+            </Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+      <input type="hidden" id={iId} value={selectedValue} />
+    </div>
+  );
 }
