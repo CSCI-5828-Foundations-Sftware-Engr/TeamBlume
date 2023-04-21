e8646a6955cbdf3498987626cc2ede4dd5da4443
Sriharsha Reddy Yarasani <71772060+sriharsharedd-y@users.noreply.github.com>
2023-04-20 22:07:18 -0600

    E2E UI Tests
    * Implementing E2E UI Tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "4qnr2a",
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
     video: false,
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
