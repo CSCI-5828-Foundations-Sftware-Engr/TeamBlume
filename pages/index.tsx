516dd95eeda1e8efe809aefe95a7f1cda94107d8
Sriharsha Reddy Yarasani <71772060+sriharsharedd-y@users.noreply.github.com>
2023-04-22 15:33:51 -0600

    Product page basic implementation
    * Product page basic implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Head from 'next/head';
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -16,20 +16,50 @@ import Router from 'next/router';
 const Home = () => {
   const sessionVar = useSession();
 
+  type dataObj = {
+    id?: number;
+    name?: string;
+    inserted_at?: string;
+    updated_at?: string;
+  };
+
+  type ddItemObj = {
+    key?: string;
+    name?: string;
+  };
+
   const [session, setSession] = useState(useSession());
   const supabase = useSupabaseClient();
-  const [showAccount, setShowAccount] = useState(false);
+
+  const[menuItems, setMenuItems] = useState<ddItemObj[]>([{key: 'select an option', name: 'select an option'}]);
+  const [data, setData] = useState(null);
 
   if (session != sessionVar) {
     setSession(sessionVar);
   }
 
-  const menuItems = [
-    { key: 'electronics', name: 'Electronics' },
-    { key: 'groceries', name: 'Groceries' }
-  ];
+  useEffect(() => {
+    fetch('/api/product/categories')
+      .then(response => response.json())
+      .then(json => 
+        {
+            setData(json); 
+            populateCategories(json.categories);
+        })
+      .catch(error => console.error(error));
+  }, []);
 
-
+  function populateCategories(data:dataObj[]) {
+    if (data) {
+      let catItems : ddItemObj[] = [];
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i].name);
+        catItems.push({ key: data[i].id?.toString(), name: data[i].name });
+      }
+      setMenuItems(catItems);
+    } 
+  }
+  
   function redirectToCompare(){
   const opVal = (document.getElementById('category-dropdown-value') as HTMLInputElement).value;
 
--- pages/pacom/compare.tsx
+++ pages/pacom/compare.tsx
@@ -1,20 +1,93 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
+
+export default () => {
+  const router = useRouter()
+  const queryObj = router.query;
+
+  const [data, setData] = useState(null);
+  const [products, setProducts] = useState<datObj[]>([]);
 
 
-export default function Compare() {
+  type dataObj = {
+    id?: number;
+    product_name?: string;
+    inserted_at?: string;
+    updated_at?: string;
+    image_link?: string;
+    product_link?: string;
+    platform?: string;
+    brand?: string;
+    category_id?: number;
+  };
 
-    const router = useRouter()
-    const queryObj = router.query;
+
+  type datObj = {
+    product_name?: string;
+    img_link?: string;
+    product_link?: string;
+    platform?: string;
+    brand?: string;
+  };
+
+  let catItems : datObj[] = [];
+
+  useEffect(() => {
+    if(queryObj!=undefined){
+    fetch('/api/product/'+queryObj.keyword)
+      .then(response => response.json())
+      .then(json => 
+        {
+            setData(json); 
+            populateData(json.products);
+        })
+      .catch(error => console.error(error));
+    }
+  }, [queryObj]);
 
 
-    return (
-      <div className="content">
-        <div className=""></div>
-        <div className="">
-          <span className="text-sm">Copyright PACom</span>
-        </div>
-        <div className="flex-end flex"></div>
-      </div>
-    );
+  function populateData(data:dataObj[]) {
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i].product_name);
+        catItems.push({ product_name: data[i].product_name?.toString(), brand: data[i].brand, img_link: data[i].image_link, product_link: data[i].product_link, platform: data[i].platform });
+      }
+      setProducts(catItems);
+      // console.log(products);
+      console.log(catItems);
+    } 
   }
-  
+
+
+  return (
+    <div className="content">
+      <div className=""></div>
+      <div className="">
+        {(products.length > 0 ? <>
+          {products.map((item, index) => (
+            <div key={index} className="flex flex-col">
+              <div className="flex flex-row">
+                <div className="flex flex-col">
+                  <img src={item.img_link} className="w-32 h-32"/>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-sm">{item.product_name}</span>
+                  <span className="text-sm">{item.brand}</span>
+                  <span className="text-sm">{item.platform}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </> 
+        : 
+        <>
+          <div className="flex flex-col">
+            No Products
+          </div>
+        </>)}
+      </div>
+      <div className="flex-end flex"></div>
+    </div>
+  );
+
+}
