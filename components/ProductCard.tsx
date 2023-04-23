3b61824c3593772cd4e7f86fd2751ec09c4fb156
ajaymopidevi <ajaymopidevi@gmail.com>
2023-04-23 03:34:50 -0600

    Capture custom events for products

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,20 @@
 import Link from 'next/link';
+import { Button } from '@nextui-org/react';
+import { usePostHog } from 'posthog-js/react';
+
 
 const ProductCard = ({ imageSrc, brand, name, productLink } : {imageSrc : string | undefined, brand : string | undefined, name : string | undefined, productLink : string}) => {
+    const posthog = usePostHog()
+
+    function productVist() {
+      posthog.capture(
+      'Product Visited',
+      {
+        name: name,
+        brand: brand
+      })
+    }
+
     return (
         <div className={'product-card'}>
           <img src={imageSrc} alt={name} />
@@ -8,8 +22,11 @@ const ProductCard = ({ imageSrc, brand, name, productLink } : {imageSrc : string
             <h3>{brand}</h3>
             <h2>{name}</h2>
             <Link href={productLink}>
-              <a>View Product</a>
+              View Product
             </Link>
+//             <Button as='a' onPress={productVist} href={productLink}>
+//                 View Product
+//             </Button>
           </div>
         </div>
       );
