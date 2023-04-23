ad073690111477c7dd84017479f5e1388c2e2155
ajaymopidevi <ajaymopidevi@gmail.com>
2023-04-23 05:32:19 -0600

    Integarted barchart to the insights

--- pages/insights.tsx
+++ pages/insights.tsx
@@ -1,19 +1,38 @@
 // pages/insights.tsx
-import { Card, Text, Metric, Title, LineChart } from "@tremor/react";
+//import { Card, Text, Metric, Title, LineChart } from "@tremor/react";
+import BarChart from '../components/BarChart'
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      data: [65, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: 'My Second dataset',
+      backgroundColor: 'rgba(0,0,255,0.4)',
+      borderColor: 'rgba(0,0,255,1)',
+
+      data: [80, 60, 40, 31, 76, 85, 80]
+    }
+
+  ]
+};
 
 export default function Insights({ count, chartData }) {
   return (
     <>
-      <Card className="max-w-xs mx-auto">
-        <Text>Insights created</Text>
-        <Metric>{count}</Metric>
-      </Card>
+      <div>
+        <BarChart data={data} />
+      </div>
     </>
   )
 }
 
 export async function getServerSideProps(context) {
-  const url = `${process.env.NEXT_PUBLIC_POSTHOG_HOST}/api/projects/${process.env.NEXT_PUBLIC_POSHOG_ID}/insights?short_id=nT97N4lg`;
+  const url = `${process.env.NEXT_PUBLIC_POSTHOG_HOST}/api/projects/${process.env.NEXT_PUBLIC_POSHOG_ID}/insights?short_id=QYJfD25z`;
 
   const request = await fetch(url, {
     method: 'GET',
@@ -25,6 +44,7 @@ export async function getServerSideProps(context) {
 
     // Return response as prop
   const response = await request.json()
+  console.log(response.results[0].result)
   const dataPoints = response.results[0].result[0].data
   const labels = response.results[0].result[0].labels
   const count=response.results[0].result[0].count
