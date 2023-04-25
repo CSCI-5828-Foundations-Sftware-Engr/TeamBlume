bb99496e5cd6f4e70bbcb2317b511a21fec9c505
ajaymopidevi <ajaymopidevi@gmail.com>
2023-04-25 00:38:02 -0600

    Edit Home page UI

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'next/router';
 
-import { Card, Grid, Text } from '@nextui-org/react';
+import { Card, Grid, Text, Spacer } from '@nextui-org/react';
 
 export const CardComponent = ({
   index,
@@ -14,7 +14,7 @@ export const CardComponent = ({
 }) => {
     return(
         <div key={index} className="flex flex-row">
-
+            <Spacer x={7} />
             <Grid>
                 <Card isPressable isHoverable
                 onPress={
@@ -31,11 +31,11 @@ export const CardComponent = ({
                     src = {"/images/"+name.toLowerCase()+".png"}
                     objectFit="cover"
                     width="fit-content"
-                    height={200}
+                    height={300}
                     alt={name}
                     />
                 <Card.Footer>
-                    <Text h2 className="primary-cat-name">{name}</Text>
+                    <Text h1 className="primary-cat-name">{name}</Text>
                 </Card.Footer>
                 </Card>
             </Grid>
--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -1,15 +1,13 @@
 export default function HomeContent({logged} : {logged : Boolean}) {
 
-  
+
   return (
     <div className={logged ? "col-12" : "col-6" }>
       {!logged ? <h1 className="Logo">PACom</h1> : <></>}
-      <p className="">
-        Welcome to Price comparison and aggregator service.
-        <br />
-        <br />
-        {(logged) ? 'Select from the below category options to start comparing prices' : 'Login or signup to get started'}
-      </p>
+      <h1>Welcome to Price comparison and aggregator service</h1>
+        <h2 className="">
+          {(logged) ? 'Select from the below category options to start comparing prices' : 'Login or signup to get started'}
+        </h2>
     </div>
   );
 }
