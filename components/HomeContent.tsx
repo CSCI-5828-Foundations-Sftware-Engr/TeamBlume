67c921ac896c64b81147ab8d50d383f3ce8f3939
ashwinhr186 <123559646+ashwinhr186@users.noreply.github.com>
2023-04-25 21:00:14 -0600

    changed UI to light theme

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -2,12 +2,10 @@ export default function HomeContent({logged} : {logged : Boolean}) {
 
   
   return (
-    <div className={logged ? "col-12" : "col-6"}>
+    <div className={logged ? "col-12" : "col-6" }>
       {!logged ? <h1 className="Logo">PACom</h1> : <></>}
       <p className="">
-        Welcome to Price comparison and aggregator service. Here you
-        will find prices of different products from different sources
-        and compare them to find the best price.
+        Welcome to Price comparison and aggregator service.
         <br />
         <br />
         {(logged) ? 'Select from the below category options to start comparing prices' : 'Login or signup to get started'}
