548e2d45132353e681b6263be8d128ba4d008578
hjoshi123 <mail2hemantjoshi@pm.me>
2023-04-25 22:18:21 -0600

    added newrelic integration

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,33 +1,43 @@
 import Link from 'next/link';
 import { Button, Text } from '@nextui-org/react';
 import { usePostHog } from 'posthog-js/react';
+import Image from 'next/image';
 
+const ProductCard = ({
+  imageSrc,
+  brand,
+  name,
+  productLink
+}: {
+  imageSrc: string;
+  brand: string | undefined;
+  name: string | undefined;
+  productLink: string;
+}) => {
+  const posthog = usePostHog();
 
-const ProductCard = ({ imageSrc, brand, name, productLink } : {imageSrc : string | undefined, brand : string | undefined, name : string | undefined, productLink : string}) => {
-    const posthog = usePostHog()
+  function productVist() {
+    posthog?.capture('Product Visited', {
+      name: name,
+      brand: brand
+    });
+  }
 
-    function productVist() {
-      posthog?.capture(
-      'Product Visited',
-      {
-        name: name,
-        brand: brand
-      })
-    }
+  return (
+    <div className={'product-card'}>
+      <Image src={imageSrc} width={300} height={300} alt={name} />
+      <div className={'product-details'}>
+        <h3>{brand}</h3>
+        <h2>{name}</h2>
 
-    return (
-        <div className={'product-card'}>
-          <img src={imageSrc} alt={name} />
-          <div className={'product-details'}>
-            <h3>{brand}</h3>
-            <h2>{name}</h2>
-
-            <Button as='a' onPress={productVist} href={productLink} auto>
-              <Text h1 size="$3x1">View Product</Text>
-            </Button>
-          </div>
-        </div>
-      );
+        <Button as="a" onPress={productVist} href={productLink} auto>
+          <Text h1 size="$3x1">
+            View Product
+          </Text>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
