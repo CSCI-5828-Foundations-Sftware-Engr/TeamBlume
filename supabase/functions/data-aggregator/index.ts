2f15658dd03648a4df16161918f7727ccf165990
rakshitha31 <rakshitha31@gmail.com>
2023-04-25 19:02:51 -0600

    updated index.ts to dynamically update data for products on change

--- supabase/functions/data-aggregator/index.ts
+++ supabase/functions/data-aggregator/index.ts
@@ -2,6 +2,7 @@
 import { scrapeBestBuy  } from "./best_buy.ts";   // <--- import the scrapeBestBuy function
 import { scrapeWalmart } from "./walmart_electronics.ts";
 import {scrapeWalmartGrocery} from "./walmart_grocery.ts";
+import { scrapeKingSoopers } from "./kingsoopers.ts";
 import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
 import { Redis } from 'https://deno.land/x/upstash_redis@v1.19.3/mod.ts'
 import { GROCERY, ELECTRONICS } from './product_list.ts';
@@ -17,38 +18,98 @@ const getPriceFromString = (str: string) => {
 
 const comparePrice = (channel:any, redis:any) => {
 
+  const channels = ["walmart_grocery", "walmart_electronics", "bestbuy", "kingsoopers"]
 
-  const channel_name = channel["1"]["platform"];
-  redis.get(channel_name).then((value:any) => {
+  for (const item of Object.keys(channel)){
     
-
-    if (value) {
-      
-      const data = JSON.parse(JSON.stringify(value));
-      let old_price = (data["1"]["price"]).toString();
-      old_price = parseFloat(old_price.slice(old_price.indexOf("$") + 1));
-
-      for (const item of Object.keys(GROCERY)) {
+    const channel_name = channel[item]["platform"];
+    redis.get(channel_name).then((value:any) => {
+      if (value) {
+        const data = JSON.parse(JSON.stringify(value));
         const new_price = getPriceFromString((channel[item]["price"]).toString());
         const old_price = getPriceFromString((data[item]["price"]).toString());
 
-         if( new_price != old_price ){
-            console.log(item);
+        const old_image = data[item]["image"];
+        const new_image = channel[item]["image"];
+
+        const old_link = data[item]["link"];
+        const new_link = channel[item]["link"];
+
+        const old_rating = data[item]["rating"];
+        const new_rating = channel[item]["rating"];
+
+        const old_numReviews = data[item]["numReviews"];
+        const new_numReviews = channel[item]["numReviews"];
+
+        if( new_price != old_price && new_price != null){
+            console.log("Price updated for " + channel_name +" " + channel[item]["title"]);
             data[item]["price"] = channel[item]["price"];
-         } else{
+         } 
+         else if( new_image != old_image && new_image != null){
+            console.log("Image updated" +" " + channel[item]["title"]);
+            data[item]["image"] = channel[item]["image"];
+          }
+          else if( new_link != old_link && new_link != null ){
+              console.log("Link updated for " + channel_name +" " + channel[item]["title"]);
+              data[item]["link"] = channel[item]["link"];
+            }    
+              else if( new_rating != old_rating && new_rating != null ){
+                  
+                  console.log("Rating updated for " + channel_name +" " + channel[item]["title"]);
+                  data[item]["rating"] = channel[item]["rating"];
+                }
+                else if( new_numReviews != old_numReviews && new_numReviews != null){
+
+                  console.log("Number of reviews updated for " + channel_name +" " + channel[item]["title"]);
+                  data[item]["numReviews"] = channel[item]["numReviews"];
+                }
+         else{
 
               console.log("no change");
          }
-
       }
-      redis.set(channel_name, JSON.stringify(data));
-      return new Response(JSON.stringify({ data }), { status: 200 })
-    }
-    else {
-      redis.set(channel_name, JSON.stringify(channel));
-    }
-});
+      else {
+        redis.set(channel_name, JSON.stringify(channel));
+      }
+    });
+
+  }
+
+  // if(channel["1"]["platform"]){
+  // const channel_name = channel["1"]["platform"];
   
+  // redis.get(channel_name).then((value:any) => {
+    
+
+  //   if (value) {
+      
+  //     const data = JSON.parse(JSON.stringify(value));
+  //     let old_price = (data["1"]["price"]).toString();
+  //     old_price = parseFloat(old_price.slice(old_price.indexOf("$") + 1));
+
+  //     for (const item of Object.keys(GROCERY)) {
+  //       const new_price = getPriceFromString((channel[item]["price"]).toString());
+  //       const old_price = getPriceFromString((data[item]["price"]).toString());
+
+  //        if( new_price != old_price ){
+  //         //  console.log(item);
+  //           data[item]["price"] = channel[item]["price"];
+  //        } else{
+
+  //             console.log("no change");
+  //        }
+
+  //     }
+      
+  //     redis.set(channel_name, JSON.stringify(data));
+  //     return new Response(JSON.stringify({ data }), { status: 200 })
+  //   }
+  
+  //   else {
+  //     redis.set(channel_name, JSON.stringify(channel));
+  //   }
+  //   });
+  // }
 }
 
 serve(async (_req) => {
@@ -57,7 +118,7 @@ serve(async (_req) => {
         // //const env = config();
         const redis = new Redis({
         url: Deno.env.get('UPSTASH_URL')!,
-        token: Deno.env.get('UPSTASH_TOKEN')!
+        token: Deno.env.get('UPSTASH_TOKEN')!,
        
       });
      
@@ -65,57 +126,35 @@ serve(async (_req) => {
       const walmart_grocery = {} as any;
       const walmart_electronics = {} as any;
       const best_buy = {} as any;
+      const kingsoopers ={} as any;
 
       for (const item of Object.keys(GROCERY)) {
-       // console.log(item);
-        // put this item in the object
+      
         walmart_grocery[item] = await scrapeWalmartGrocery(GROCERY[item]);
+        kingsoopers[item] = await scrapeKingSoopers(GROCERY[item]);
+
     }
 
       for(const item of Object.keys(ELECTRONICS)){
+
         walmart_electronics[item] = await scrapeWalmart(ELECTRONICS[item]);
         best_buy[item] = await scrapeBestBuy(ELECTRONICS[item]);
+
       }
 
+      console.log(kingsoopers);
+    //  console.log(walmart_grocery);
+    //  console.log(walmart_electronics);
+    //  console.log(best_buy);
 
-        // redis.get('walmart_grocery').then((value) => {
-
-        //     if (value) {
-              
-        //       const data = JSON.parse(JSON.stringify(value));
-        //       let old_price = (data["1"]["price"]).toString();
-        //       old_price = parseFloat(old_price.slice(old_price.indexOf("$") + 1));
-
-        //       for (const item of Object.keys(GROCERY)) {
-        //         const new_price = getPriceFromString((walmart_grocery[item]["price"]).toString());
-        //         const old_price = getPriceFromString((data[item]["price"]).toString());
-
-        //          if( new_price != old_price ){
-        //             console.log(item);
-        //             data[item]["price"] = walmart_grocery[item]["price"];
-                    
-
-        //          } else{
-
-        //               console.log("no change");
-        //          }
-
-        //       }
-        //       redis.set('walmart_grocery', JSON.stringify(data));
-        //       return new Response(JSON.stringify({ data }), { status: 200 })
-        //     }
-        //     else {
-        //       redis.set('walmart_grocery', JSON.stringify(walmart_grocery));
-        //     }
-        // });
         const walmart_grocery_data = await comparePrice(walmart_grocery, redis);
         const walmart_electronics_data = await comparePrice(walmart_electronics, redis);
         const best_buy_data = await comparePrice(best_buy, redis);
+        const kingsoopers_data = await comparePrice(kingsoopers, redis);
 
-        console.log(walmart_grocery_data, walmart_electronics_data, best_buy_data);
-      
-        
-        
+       
+     //   console.log(walmart_grocery_data, walmart_electronics_data, best_buy_data);
+ 
       return new Response(JSON.stringify({ walmart_grocery }), { status: 200 })
     
     } catch (error) {
--- supabase/functions/data-aggregator/kingsoopers.ts
+++ supabase/functions/data-aggregator/kingsoopers.ts
@@ -1,12 +1,15 @@
 import axiod from 'https://deno.land/x/axiod@0.26.2/mod.ts';
 import * as cheerio from 'https://esm.sh/cheerio@1.0.0-rc.12';
 
-export const scrapeWalmart = async (search: any) => {
+// deno-lint-ignore no-explicit-any
+export const scrapeKingSoopers = async (search: any) => {
   try {
+    console.log('searching kingsoopers for ' + search)
     
     const result = await axiod.get(
-
-      'https://www.walmart.com/search?q=' + search,
+    
+      'https://www.kingsoopers.com/search?query=' + search,
+     
       {
         headers: {
           'Accept-enconding': 'application/gzip',
@@ -31,26 +34,20 @@ export const scrapeWalmart = async (search: any) => {
     const products: Product[] = [];
 
     // deno-lint-ignore no-explicit-any
-    $('.mb0.ph1.pa0-xl.bb.b--near-white.w-25').each((_: any, element: any) => {
-      if (products.length >= 1) return false;
-      const image = $(element).find('img.absolute.top-0.left-0').attr('src')!;
-      const link = $(element)
-        .find('a.absolute.w-100.h-100.z-1.hide-sibling-opacity')
-        .attr('href');
-      const title = $(element)
-        .find('a.absolute.w-100.h-100.z-1.hide-sibling-opacity > span')
-        .text();
+    $(' .AutoGrid-cell.min-w-0 ').each((_: any, element: any) => {
+      console.log("here");
+    if (products.length >= 1) return false;
+      const image = $(element).find('img.kds-Image-img').attr('src')!;
+     // console.log("here");
+      const link = $(element).find('a.kds-Link.kds-Link--inherit.kds-Link--implied.ProductDescription-truncated.overflow-hidden.text-primary').attr('href');
+      const title = $(element).find('h3.kds-Text--l.text-primary.font-secondary.font-medium.my-8').text();
       const price = $(element)
-        .find('div.flex.flex-wrap.justify-start.items-center.lh-title.mb1 > div.mr1.mr2-xl.b.black.green.lh-copy.f5.f4-l')
+        .find(
+          'data.kds-Price.kds-Price--alternate[aria-label] > '
+        )
         .text();
-      const rating = $(element)
-        .find('div.flex.items-center.mt2 > span.w_iUH7')
-        .text()
-        .split(' ')[0];
-      const numReviews = $(element)
-        .find('div.flex.items-center.mt2 > span[aria-hidden="true"]')
-        .text();
-
+      const rating = $(element).find('p.visually-hidden').text();
+      const numReviews = rating.split(' ')[7];
       const scraped: {
         platform: string;
         title: string;
@@ -60,21 +57,23 @@ export const scrapeWalmart = async (search: any) => {
         rating: string;
         numReviews: string;
       } = {
-        platform: 'walmart_electronics',
+        platform: 'kingsoopers',
         title: title,
         price: price,
         image: image,
-        link: 'https://walmart.com' + link,
+        link: 'https://www.kingsoopers.com'+ link,
         rating: rating,
         numReviews: numReviews
       };
       products.push(scraped);
+      console.log(scraped);
     });
-    // console.log(products);
+    
     return new Promise((resolve, _reject) => {
       resolve(products[0]);
     });
+
   } catch (error) {
-    console.log(error);
+    console.log('ERROR' + error);
   }
 };
