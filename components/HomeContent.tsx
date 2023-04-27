391061631ab82cf0c003a3c5ddc6688ee25ec5ab
ashwinhr186 <123559646+ashwinhr186@users.noreply.github.com>
2023-04-26 22:11:29 -0600

    fix in home page

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -4,12 +4,11 @@ export default function HomeContent({logged} : {logged : Boolean}) {
   return (
     <div className={logged ? "col-12" : "col-6" }>
       {!logged ? <h1 className="Logo">PACom</h1> : <></>}
-      <p className="">
-        Welcome to Price comparison and aggregator service.
-        <br />
+     <h1>Welcome to Price comparison and aggregator service</h1>
+     <h2 className="">
         <br />
         {(logged) ? 'Select from the below category options to start comparing prices' : 'Login or signup to get started'}
-      </p>
+      </h2>
     </div>
   );
 }
